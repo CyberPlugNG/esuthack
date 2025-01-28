fa1f779a2617b256e269cf3b1776ae6474fb6c99
CyberPlugNG <cyberplugng@gmail.com>
2025-01-28 17:15:35 +0100

    Update esuthackscript.py

--- esuthackscript.py
+++ esuthackscript.py
@@ -1,13 +1,12 @@
 import os
-import pandas as pd
 import csv
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-import requests
 
 def login_and_get_profile_info(username, password, chromedriver_path):
     # Set the path to the chromedriver
